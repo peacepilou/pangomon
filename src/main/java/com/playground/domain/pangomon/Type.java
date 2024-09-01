f7450e148778ae939346f98c85a619cb09649da3
BenoitDeligny <delignyb.pro@gmail.com>
2024-09-01 14:33:40 +0200

    Add types
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/com/playground/domain/pangomon/Type.java
@@ -0,0 +1,22 @@
+package com.playground.domain.pangomon;
+
+public enum Type {
+    NORMAL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    DARK,
+    STEEL,
+    FAIRY
+}
